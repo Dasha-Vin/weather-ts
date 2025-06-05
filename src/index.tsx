bebb1c97d95d6eae019fa696405ff06d9707c9b7
Даша <dashavinaeva98gmail.com@mail.ru>
2025-06-05 09:09:40 +0500

    RTK

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './components/store';
-import './index.css';
+import { store } from './components/store'; // Проверьте путь
 import App from './App';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
