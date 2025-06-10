6cf3b376d724447ee4497723a236bd206bba8fdc
Даша <dashavinaeva98gmail.com@mail.ru>
2025-06-10 14:55:23 +0500

    day

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,7 @@ const root: ReactDOM.Root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-<<<<<<< HEAD
 initTheme();
-=======
-// Функция для применения темы к документу
-const applyTheme = (theme: 'light' | 'dark') => {
-  document.documentElement.className = theme + '-theme';
-  localStorage.setItem('theme', theme);
-};
-
-store.subscribe(() => {
-  const state = store.getState();
-  applyTheme(state.theme);
-});
-
-applyTheme(savedTheme);
->>>>>>> 6b4ab780043976b24d8d15dfb547cf127099e850
 
 root.render(
   <React.StrictMode>
