c24120ae60447de95d1cd46fe036050e3c33fe88
Даша <dashavinaeva98gmail.com@mail.ru>
2025-06-12 18:48:11 +0500

    Updates

--- src/components/form/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IFormInputs {
-  city: string;
-  remember: boolean;
-}
-
-// Типизация пропсов для компонента Form
-export interface FormProps {
-  onFormSubmit: (data: { city: string }) => void;
-}
