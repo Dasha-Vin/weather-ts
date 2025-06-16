9673f68be6c473d8a2fa765182866039ea02c9fd
Даша <dashavinaeva98gmail.com@mail.ru>
2025-06-16 13:48:33 +0500

    Updates

--- src/components/weather/types.ts
+++
@@ -1,9 +0,0 @@
-// Типизация пропсов для компонента Weather
-export interface WeatherProps {
-  city?: string;
-  country?: string;
-  temp?: number;
-  pressure?: number;
-  sunset?: string;
-  error?: string;
-}
